48eeea8ff4dc1bf29b70cf77aa1dbe535aef56e8
Francisco Perez <fperezfrancisco4@gmail.com>
2023-10-30 19:42:06 -0700

    fixed button functionality and stats

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
 /** set up game */
+//search for a new word
+
+let randomWord = {
+  url: "https://random-word-api.p.rapidapi.com/L/",
+  options: {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "3802ae3cccmsh5d62e521d884914p1d4cafjsn253bdc5e8ed6",
+      "X-RapidAPI-Host": "random-word-api.p.rapidapi.com",
+    },
+  },
+  fetchWord: function (length) {
+    /*
+    try {
+      const response = (await fetch(this.url + `${length}`, this.options)).json();
+      const result = await response.text();
+      console.log(result);
+      //console.log(response.word);
+    } catch (error) {
+      console.error(error);
+    }*/
+    fetch(this.url + `${length}`, this.options)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Data: ", data);
+        return this.setGameWord(data.word);
+      });
+  },
+  setGameWord: function (word) {
+    gameWord = word;
+    console.log(gameWord);
+  },
+};
 
 //important variables
 const wordGrid = document.querySelector(".wordGrid");
@@ -35,6 +70,45 @@ const wordList = [
   "water",
 ];
 
+/**  set up stats */
+let playerStats = getLocalStorage("playerStats");
+
+if (!playerStats) {
+  //set up player stats
+  resetStats();
+}
+
+console.log(playerStats);
+
+const numGamesPlayed = document.getElementById("numGamesPlayed");
+const numGamesWon = document.getElementById("numGamesWon");
+const percentWonNumber = document.getElementById("percentWonNumber");
+const currStreakNumber = document.getElementById("currStreakNumber");
+const largestStreakNumber = document.getElementById("largestStreakNumber");
+
+function setUpStats() {
+  numGamesPlayed.textContent = playerStats.gamesPlayed;
+  numGamesWon.textContent = playerStats.gamesWon;
+  percentWonNumber.textContent = playerStats.percentWon;
+  currStreakNumber.textContent = playerStats.currentStreak;
+  largestStreakNumber.textContent = playerStats.longestStreak;
+}
+
+const resetBtn = document.getElementById("resetBtn");
+resetBtn.addEventListener("click", () => {
+  //handle reset
+  exitMessage(4);
+  let clientAnswer = confirm(
+    "Are you sure you want to reset your stats? (cannot be undone.)"
+  );
+  if (clientAnswer) {
+    resetStats();
+    setUpStats();
+  }
+});
+
+/**  set up stats */
+
 var gameWord;
 const messageBox = document.querySelector(".messageBox");
 const startGameMessage = document.getElementById("startGame");
@@ -50,11 +124,27 @@ newGameBtn.addEventListener("click", newGameSetUp);
 const deleteBtn = document.getElementById("delete");
 deleteBtn.addEventListener("click", deleteLetter);
 
+const deleteMobileBtn = document.getElementById("deleteMobile");
+deleteMobileBtn.addEventListener("click", deleteLetter);
+
 document.getElementById("exitStart").addEventListener("click", exitBox);
 document.getElementById("exitGuess").addEventListener("click", exitBox);
 document.getElementById("exitGameOver").addEventListener("click", exitBox);
+document.getElementById("exitStats").addEventListener("click", exitBox);
 
 const p = document.getElementById("lossMessage");
+
+const giveUpBtn = document.getElementById("giveUpBtn");
+giveUpBtn.addEventListener("click", gameOver);
+
+const statsBtn = document.getElementById("statsBtn");
+const statsBox = document.getElementById("statsBox");
+statsBtn.addEventListener("click", () => {
+  statsBox.classList.remove("hidden");
+  messageBox.classList.remove("hidden");
+});
+
+/*** FUNCTION TO BEGIN A NEW GAME */
 
 function beginNewGame() {
   //set all global variables
@@ -62,6 +152,10 @@ function beginNewGame() {
     item.addEventListener("click", keyClick);
     item.classList.remove("used");
   });
+
+  window.addEventListener("keydown", keyDown);
+
+  setUpStats();
 
   p.textContent = "";
 
@@ -72,13 +166,16 @@ function beginNewGame() {
   cellIndex = 1;
   rowIndex = 1;
   //select primary guessing word
-  gameWord = wordList[Math.floor(Math.random() * wordList.length)];
+  //gameWord = wordList[Math.floor(Math.random() * wordList.length)];
+  randomWord.fetchWord(5);
+  //gameWord = randomWord.fetchWord(5);
   //const gameWord = wordList[Math.floor(Math.random() * wordList.length)];
   console.log(gameWord);
+
   gameWon = false;
   setTimeout(function () {
     exitMessage(1);
-  }, 5000);
+  }, 3000);
 }
 
 function exitBox(event) {
@@ -89,6 +186,8 @@ function exitBox(event) {
     return exitMessage(2);
   } else if (currMessage.id === "gameOver") {
     return exitMessage(3);
+  } else if (currMessage.id === "statsBox") {
+    return exitMessage(4);
   }
 }
 
@@ -97,8 +196,10 @@ function exitMessage(index) {
     startGameMessage.classList.add("hidden");
   } else if (index === 2) {
     incompleteGuess.classList.add("hidden");
-  } else {
+  } else if (index === 3) {
     gameOverMessage.classList.add("hidden");
+  } else {
+    statsBox.classList.add("hidden");
   }
   messageBox.classList.add("hidden");
 }
@@ -163,8 +264,22 @@ function keyClick(event) {
   }
 }
 
+function keyDown(event) {
+  const key = event.key;
+  console.log(key);
+  if (key === "Enter") {
+    rowCheck(gameWord);
+  } else if (key === "Delete" || key === "Backspace") {
+    return deleteLetter();
+  } else if (key === "" || key === null || key.length > 1) {
+    /**do nothing */
+  } else {
+    return addLetter(key);
+  }
+}
+
 function rowCheck(currWord) {
-  if (currWord.length != 5) {
+  if (currWord.length !== 5) {
     //incomplete guess warning
     console.log("chosen word is not long enough");
   } else if (cellIndex <= 5) {
@@ -232,12 +347,29 @@ function nextRowSet() {
 
 function gameOver() {
   if (!gameWon) {
+    playerStats.currentStreak = 0;
+    //adjust stats
     gameResultMessage.textContent = "You Lost!";
     p.textContent = "The word was: " + gameWord;
     //gameOverMessage.appendChild(p);
   } else {
+    //adjust stats
+    playerStats.currentStreak += 1;
+    playerStats.gamesWon += 1;
+    if (playerStats.currentStreak > playerStats.longestStreak) {
+      playerStats.longestStreak = playerStats.currentStreak;
+    }
+    //adjust stats
     gameResultMessage.textContent = "You Won!";
   }
+  //adjust stats
+  playerStats.gamesPlayed += 1;
+  playerStats.percentWon = (
+    playerStats.gamesWon / playerStats.gamesPlayed
+  ).toFixed(2);
+
+  console.log(playerStats);
+  setLocalStorage(playerStats, "playerStats");
 
   const currMessage = gameResultMessage.parentNode;
   currMessage.classList.remove("hidden");
@@ -254,6 +386,7 @@ function newGameSetUp() {
     item.classList.remove("wrong");
     item.classList.remove("wrongPlacement");
   });
+  playerStats = getLocalStorage("playerStats");
   //close out message
   exitMessage(3);
   //show beginning message
@@ -261,6 +394,36 @@ function newGameSetUp() {
   messageBox.classList.remove("hidden");
   //start new game
   return beginNewGame();
+}
+
+//save to local storage
+function setLocalStorage(obj, key) {
+  // Serialize the object using JSON.stringify()
+  const serializedObj = JSON.stringify(obj);
+  // Store the serialized object in localStorage
+  localStorage.setItem(key, serializedObj);
+}
+
+//retrieve from local storage
+
+function getLocalStorage(key) {
+  // Retrieve the serialized object from localStorage
+  const serializedObj = localStorage.getItem(key);
+  // Deserialize the object using JSON.parse()
+  const obj = JSON.parse(serializedObj);
+  return obj;
+}
+
+function resetStats() {
+  //set up player stats
+  playerStats = {
+    gamesPlayed: 0,
+    gamesWon: 0,
+    percentWon: 0,
+    currentStreak: 0,
+    longestStreak: 0,
+  };
+  setLocalStorage(playerStats, "playerStats");
 }
 
 //run initial set up function
